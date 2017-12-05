2ae48b2dea20362ea123ab50137b980354c01bfc
richard <shihxuancheng@gmail.com>
2017-12-05 17:18:56 +0800

    Modify script

--- wild-docker-files/remove.sh
+++ wild-docker-files/remove.sh
@@ -2,7 +2,6 @@ docker rm -f wild1
 docker rm -f wild2
 docker rm -f wild3
 docker rm -f wild-balancer
-docker rmi -f wild1
-docker rmi -f wild2
-docker rmi -f wild3
-docker rmi -f wild-balancer
+docker rmi -f wildfly-cluster
+docker rmi -f jboss/wildfly
+docker rmi -f jasonwyatt/nginx-loadbalancer
