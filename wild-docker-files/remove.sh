1f2b244f65665422a6211f79f8fc6d45c0116251
richard <shihxuancheng@gmail.com>
2017-12-05 16:16:00 +0800

    Add script with more functions

--- wild-docker-files/remove.sh
+++ wild-docker-files/remove.sh
@@ -2,3 +2,7 @@ docker rm -f wild1
 docker rm -f wild2
 docker rm -f wild3
 docker rm -f wild-balancer
+docker rmi -f wild1
+docker rmi -f wild2
+docker rmi -f wild3
+docker rmi -f wild-balancer
---
+++ wild-docker-files/stop.sh
@@ -0,0 +1,4 @@
+docker stop wild1
+docker stop wild2
+docker stop wild3
+docker stop wild-balance
